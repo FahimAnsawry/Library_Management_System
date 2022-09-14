9b5847860c67d3341d727b404259875402d98d8e
AH-Ratul <ahratul21@gmail.com>
2022-09-14 19:25:40 +0600

    git init

--- src/main/java/com/example/librarymanagementsystem/App.java
+++ src/main/java/com/example/librarymanagementsystem/App.java
@@ -3,21 +3,25 @@ package com.example.librarymanagementsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("HomePage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Library Management System");
         stage.setScene(scene);
+        stage.resizableProperty().setValue(false);  // fixed the window
+        stage.getIcons().add(new Image(Objects.requireNonNull(App.class.getResourceAsStream("bookshelf.png"))));
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args);  // launch the app
     }
 }
